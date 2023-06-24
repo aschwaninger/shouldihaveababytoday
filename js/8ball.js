25d3b31e51a63a7f9b2710801d8ecc5078eda948
aschwaninger <admiralducksauce@gmail.com>
2023-06-23 22:57:26 -0400

    Update 8ball.js

--- js/8ball.js
+++ js/8ball.js
@@ -3,4 +3,6 @@ async function getFortune() {
   const response = await fetch("https://gb7k1ibk31.execute-api.us-east-1.amazonaws.com/8ball");
   const fortune = await response.json();
   console.log(fortune);
+  document.getElementById("answer").innerHTML = fortune.answer;
+  document.getElementById("reason").innerHTML = fortune.reason;
 }
